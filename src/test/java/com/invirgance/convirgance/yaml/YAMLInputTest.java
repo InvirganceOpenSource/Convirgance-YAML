329d5610d72c7adae1931071320dd3c0d9ec52ca
Timur Kasimov <kasimovmoor@gmail.com>
2025-03-27 14:33:20 -0500

    testing an empty yaml file

--- src/test/java/com/invirgance/convirgance/yaml/YAMLInputTest.java
+++ src/test/java/com/invirgance/convirgance/yaml/YAMLInputTest.java
@@ -8,6 +8,8 @@ import com.invirgance.convirgance.input.InputCursor;
 import com.invirgance.convirgance.json.JSONObject;
 import com.invirgance.convirgance.source.ClasspathSource;
 import com.invirgance.convirgance.source.Source;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,6 +29,9 @@ public class YAMLInputTest
     @Test
     public void testRead()
     {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+      
         YAMLInput input = new YAMLInput();
         ClasspathSource source = new ClasspathSource("/data.yaml");
         
@@ -35,13 +40,38 @@ public class YAMLInputTest
             System.out.println(record);
         }
         
+        String expected = "{\"name\":\"John Doe\",\"age\":30,\"email\":\"johndoe@example.com\"}\n" +
+                          "{\"name\":\"Jane Smith\",\"age\":25,\"email\":\"janesmith@example.com\"}\n" +
+                          "{\"name\":\"Alice Johnson\",\"age\":40,\"email\":\"alice@example.com\"}\n";
+        
+        assertEquals(expected, outContent.toString());
+        
     }
     
-    // clean up github: target and .DS_Store with gitignore
-    
-    // empty yaml file
+    /**
+     * Test read method on empty YAML file.
+     */
+    @Test
+    public void testEmpty()
+    {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        
+        YAMLInput input = new YAMLInput();
+        ClasspathSource source = new ClasspathSource("/empty.yaml");
+        
+        for (JSONObject record : input.read(source))
+        {
+            System.out.println(record);
+        }
+        
+        assertEquals("", new ByteArrayOutputStream().toString());
+        
+    }
     
     // only one object in file
+    
+   
     
     // multiple files
     
